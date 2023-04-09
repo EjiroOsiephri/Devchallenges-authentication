6fa2abcf1dd1fd41e9d82929b911f323b88bfcf2
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-10 00:24:28 +0100

    styling the signup page

--- src/Routes/Signup.jsx
+++ src/Routes/Signup.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
-import '../components/sass/signup.module.scss'
+import Styled from '../components/sass/signup.module.scss'
 import Logo from "../components/images/devchallenges.svg"
+import googleImage from "../components/images/Google.svg"
+import githubImage from "../components/images/Gihub.svg"
+import facebookImage from "../components/images/Facebook.svg"
+import twitterImage from "../components/images/Twitter.svg"
 
 const Signup = () => {
     return (
-        <div className="container">
-            <div className="img">
-                <img src={Logo} alt="" />
-                <h2>devchallenges</h2>
-            </div>
-            <div className="typography">
-                <h3>Join thousands of learners from around the world </h3>
-                <h4>Master web development by making real-life projects. There are multiple paths for you to choose</h4>
-            </div>
-            <div className="input-container">
-                <input type="text" />
-                <input type="password" />
+        <div className={Styled.container}>
+            <div className={Styled.subContainer}>
+                <div className={Styled.img}>
+                    <img src={Logo} alt="" />
+                </div>
+                <div className={Styled.typography}>
+                    <h3>Join thousands of learners from around the world </h3>
+                    <h4>Master web development by making real-life projects. There are multiple paths for you to choose</h4>
+                </div>
+                <div className="input-container">
+                    <input type="text" />
+                    <input type="password" />
+                </div>
+                <div className="button">
+                    <button>Start coding now</button>
+                </div>
+                <p>or continue with these social profile</p>
+                <div className="extraLoginDiv">
+                    <img src={googleImage} alt="" />
+                    <img src={githubImage} alt="" />
+                    <img src={facebookImage} alt="" />
+                    <img src={twitterImage} alt="" />
+                </div>
             </div>
         </div>
     )
