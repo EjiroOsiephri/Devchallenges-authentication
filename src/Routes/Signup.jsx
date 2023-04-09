589bad4958a906403e578fe4152bb51e60cff730
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-09 15:02:50 +0100

    Styling the signup page

--- src/Routes/Signup.jsx
+++ src/Routes/Signup.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../components/sass/signup.module.scss'
+import Logo from "../components/images/devchallenges.svg"
 
 const Signup = () => {
     return (
-        <div>Signup</div>
+        <div className="container">
+            <div className="img">
+                <img src={Logo} alt="" />
+                <h2>devchallenges</h2>
+            </div>
+            <div className="typography">
+                <h3>Join thousands of learners from around the world </h3>
+                <h4>Master web development by making real-life projects. There are multiple paths for you to choose</h4>
+            </div>
+            <div className="input-container">
+                <input type="text" />
+                <input type="password" />
+            </div>
+        </div>
     )
 }
 
