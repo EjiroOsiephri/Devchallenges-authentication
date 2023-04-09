d92f21dd340a0272f94e2dd7a1d6a5327062be77
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-09 14:49:29 +0100

    setting up private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 import { Route, Routes } from "react-router-dom"
 import AuthContext from './context/AuthContext'
+import Login from './Routes/Login'
+import Signup from './Routes/Signup'
 
 function App() {
+  const value = {
 
+  }
 
   return (
     <>
-      <AuthContext.Provider>
+      <AuthContext.Provider value={value}>
         <Routes>
+          <Route path='/' element={<Signup></Signup>}></Route>
           <Route></Route>
         </Routes>
       </AuthContext.Provider>
