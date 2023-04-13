91e1e8f8cfd01982b0f680972e498659adfbd28e
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-04-13 16:36:51 +0100

    Setting up img display

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import NavBar from './NavBar'
-import { getDocs, collection, setDoc } from "firebase/firestore"
+import { getDocs, collection, setDoc, doc } from "firebase/firestore"
 import { useState, useEffect } from 'react'
 import { Firestore, auth } from '../Firebase/Firebase'
 import Styled from "../components/sass/EditPage.module.scss"
+import person from "../components/images/download.png"
+import { Link } from 'react-router-dom'
 
 const Edit = () => {
    const [authPage, setAuthPage] = useState([])
    const [updateTitle, setUpdatedTitle] = useState('')
    const [updateBio, setUpdatedBio] = useState('')
-   const [updateIMG, setUpdatedIMG] = useState('')
+   const [updatePhone, setUpdatedPhone] = useState('')
+   const [updateEmail, setUpdatedEmail] = useState('')
+   const [updatePassword, setUpdatedPassword] = useState('')
 
 
    const authCollectionList = collection(Firestore, "authDevChallenges")
@@ -35,30 +39,58 @@ const Edit = () => {
       const movieDoc = doc(Firestore, "authDevChallenges", auth.currentUser.uid)
       await setDoc(movieDoc, {
          Name: updateTitle,
-         Bio: updateBio,
-         IMG: updateIMG
+         BIO: updateBio,
+         phone: updatePhone,
+         EMAIL: updateEmail,
+         password: updatePassword
       })
       getAuthData()
    }
 
    return (
       <>
+         <Link to="/details"></Link>
          <NavBar></NavBar>
-         <div className={Styled["text-container"]}>
-            <h3>Profile</h3>
-            <h6>Some info may be visible to other people</h6>
-         </div>
-         <div>
-            <input type="text" onChange={(e) => {
-               setUpdatedTitle(e.target.value)
-            }} />
-            <input type="text" onChange={(e) => {
-               setUpdatedBio(e.target.value)
-            }} />
-            <input type="text" onChange={(e) => {
-               setUpdatedIMG(e.target.value)
-            }} />
-            <button onClick={() => updateMovieTitle()}>Update movie title</button>
+         <div className={Styled["allContainer"]}>
+            <div className={Styled["img"]}>
+               <img className={Styled['person']} src={person} alt="" />
+               <h4>Change Photo</h4>
+            </div>
+            <div className={Styled["text-container"]}>
+               <h3>Profile</h3>
+               <h6>Some info may be visible to other people</h6>
+            </div>
+            <div className={Styled['name']}>
+               <label htmlFor="">NAME</label>
+               <input type="text" onChange={(e) => {
+                  setUpdatedTitle(e.target.value)
+               }} />
+            </div>
+            <div className={Styled["bio"]}>
+               <label htmlFor="">BIO</label>
+               <input type="text" onChange={(e) => {
+                  setUpdatedBio(e.target.value)
+               }} />
+            </div>
+            <div className={Styled["phone"]}>
+               <label htmlFor="">PHONE</label>
+               <input type="text" onChange={(e) => {
+                  setUpdatedPhone(e.target.value)
+               }} />
+            </div>
+            <div className={Styled["email"]}>
+               <label htmlFor="">EMAIL</label>
+               <input type="text" onChange={(e) => {
+                  setUpdatedEmail(e.target.value)
+               }} />
+            </div>
+            <div className={Styled["password"]}>
+               <label htmlFor="">PASSWORD</label>
+               <input type="text" onChange={(e) => {
+                  setUpdatedPassword(e.target.value)
+               }} />
+            </div>
+            <button onClick={() => updateMovieTitle()}>Save</button>
          </div>
       </>
 
